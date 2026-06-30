b60678c9b50e67c883b6102c868bd2df7a67e61e
mutvayzz-sys <mutvayzz@gmail.com>
2026-06-30 13:49:47 +0400

    chore: remove _archive/ — tracked stale docs + old build artifacts (384 files)

--- _archive/2026-06-24_prune/out-build2/win-unpacked/resources/bundled-aioncore/win32-x64/managed-resources/node/node-v24.11.0-win-x64/install_tools.bat
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8719afe6ec81742a9a5ea30ee4801249e3a80c33279b3bfe2016e6eed9b750e3
-size 3094
--- _archive/2026-06-24_prune/out-build2/win-unpacked/resources/sw.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:405ada7a89ceaa446d0eb82031128b76c238707d86d4aba3d3956d66a8aa6283
-size 5561
